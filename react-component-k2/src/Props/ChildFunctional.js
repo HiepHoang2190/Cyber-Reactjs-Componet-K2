943efc853441bc5953a2b7493ce7111f486553b8
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-07-03 10:14:02 +0700

    ReactJs-K2 lesson 2 :Truyền dữ liệu thông qua props

--- react-component-k2/src/Props/ChildFunctional.js
+++ react-component-k2/src/Props/ChildFunctional.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 export default function ChildFunctional(props) {
+
+    let { src, name, price } = props.productItem;
     return (
         <div>
-            <img style={{ width: 50 }} src={props.propSource} />
+            <img style={{ width: 50 }} src={src} />
             <div style={{ width: 125 }} className="card text-left">
-                <img style={{ width: 25 }} className="card-img-top" src={props.propSource} alt={props.propSource} />
+                <img style={{ width: 25 }} className="card-img-top" src={src} alt={src} />
                 <div className="card-body">
-                    <h4 className="card-title">{props.name}</h4>
-                    <p className="card-text">Text</p>
+                    <h4 className="card-title">{name}</h4>
+                    <p className="card-text">{price}</p>
                 </div>
             </div>
         </div>
--- react-component-k2/src/Props/Parent.js
+++ react-component-k2/src/Props/Parent.js
@@ -4,29 +4,34 @@ import ChildFunctional from './ChildFunctional';
 
 class Parent extends Component {
 
-    src = "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/464e8d65-3a82-472a-aa2c-de53b2dfe7f2/wearallday-shoe-XpS7nX.png";
-    name = "nike air max"
+    product = {
+        id: 1,
+        name: "nike air max",
+        src: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/464e8d65-3a82-472a-aa2c-de53b2dfe7f2/wearallday-shoe-XpS7nX.png",
+        desc: 'Giày nhập ngoại',
+        price: 1000,
+        size: [36, 37, 38, 39, 40]
 
+    }
+
+    size = [36, 37, 38, 39, 40]
+
+    showInfo = (name) => {
+        alert(name)
+    };
     render() {
         return (
             <div>
                 <input type="text" />
-                {/* <Child propSource={this.src}
-                    name={this.name}
-                />
-                <Child propSource={this.src}
-                    name={'nike air max x2'}
-                />
-                <Child propSource={this.src}
-                    name={'nike air max x4'}
-                />
-                <Child propSource={this.src}
-                    name={'nike air max x555'}
-                /> */}
 
+                <Child
+                    productItem={this.product}
+                    shoesSize={this.size}
+                    showAlert={this.showInfo}
+                />
                 {/* ChildFunctional */}
-                <ChildFunctional propSource={this.src}
-                    name={this.name} />
+                {/* <ChildFunctional productItem={this.product}
+                /> */}
             </div>
         );
     }

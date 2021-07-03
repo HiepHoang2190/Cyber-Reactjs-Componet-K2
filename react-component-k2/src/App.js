90edbf9cfb1f716cee39bf469743e17abc9c0c19
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-07-03 23:02:35 +0700

    ReactJs-K2 lesson 9 : khái niệm props.children và ref

--- react-component-k2/src/App.js
+++ react-component-k2/src/App.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 // import DemoProps from './Props/DemoProps';
 // import ProductList from './Props/ProductList';
 import dataJson from './Data/data.json';
+import DemoProps from './Props/DemoProps';
 import ExerciseCarStore from './Props/ExerciseCarStore/ExerciseCarStore';
+import Parent from './Props/Parent';
+import DanhSachSanPham from './Props/DanhSachSanPham'
 // import DanhSachSanPham from './Props/DanhSachSanPham';
 class App extends Component {
+  refParent = React.createRef();
+  changeTitle = () => {
+    this.refParent.current.changeTitle();
+  }
   render() {
+
     return (
       <div>
-
-        <ExerciseCarStore />
+        <button onClick={this.changeTitle}>Change title</button>
+        <Parent ref={this.refParent}>
+          <DanhSachSanPham />
+          <h3>Hello cyber learn</h3>
+          <DanhSachSanPham />
+          <DanhSachSanPham />
+        </Parent>
       </div>
 
     );
--- react-component-k2/src/Props/Parent.js
+++ react-component-k2/src/Props/Parent.js
@@ -4,34 +4,22 @@ import ChildFunctional from './ChildFunctional';
 
 class Parent extends Component {
 
-    product = {
-        id: 1,
-        name: "nike air max",
-        src: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/464e8d65-3a82-472a-aa2c-de53b2dfe7f2/wearallday-shoe-XpS7nX.png",
-        desc: 'Giày nhập ngoại',
-        price: 1000,
-        size: [36, 37, 38, 39, 40]
+    state = { title: 'cyberlearn' }
 
+    changeTitle = () => {
+        this.setState({
+            title: 'Khải'
+        })
     }
-
-    size = [36, 37, 38, 39, 40]
-
-    showInfo = (name) => {
-        alert(name)
-    };
     render() {
         return (
             <div>
-                <input type="text" />
-
-                <Child
-                    productItem={this.product}
-                    shoesSize={this.size}
-                    showAlert={this.showInfo}
-                />
-                {/* ChildFunctional */}
-                {/* <ChildFunctional productItem={this.product}
-                /> */}
+                <h1>{this.state.title}</h1>
+                {/* <button onClick={this.changeTitle}>Change title</button> */}
+                {/* <h3>Danh sách sản phẩm</h3>
+                {this.props.children.map((com, index) => {
+                    return com;
+                })} */}
             </div>
         );
     }

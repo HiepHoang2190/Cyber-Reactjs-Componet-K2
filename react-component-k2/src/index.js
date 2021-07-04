9629f61d764758b5c0ce1ab62097177ba40b2b2c
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-07-04 15:16:00 +0700

    ReactJs-K2 lesson 14: hướng dẫn setup redux: npm i redux

--- react-component-k2/src/index.js
+++ react-component-k2/src/index.js
@@ -4,9 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+//setup redux
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './redux/rootReducer';
 
-  <App />
+const store = createStore(rootReducer);
+// const store = createStore(rootReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+
   ,
   document.getElementById('root')
 );
---
+++ react-component-k2/src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+
+const rootReducer = combineReducers({ //store tổng của ứng dụng
+
+})
+
+export default rootReducer;

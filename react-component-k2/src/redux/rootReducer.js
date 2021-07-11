6212420b69b247974ae695d1a4c9acccdd526295
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-07-11 11:32:35 +0700

    ReactJs-K2 lesson 24:Bài tập game oẳn tù xì- binding state redux về component(phần 2 )

--- react-component-k2/src/redux/rootReducer.js
+++ react-component-k2/src/redux/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import BaiTapGioHangReducer from '../redux/BaiTapGioHangReducer'
 import BaiTapGameXucXacReducer from '../redux/BaiTapXucXacReducer'
-
+import BaiTapOanTuXiReducer from './BaiTapOanTuXiReducer'
 const rootReducer = combineReducers({ //store tổng của ứng dụng
     stateGioHang: BaiTapGioHangReducer,  // state giỏ hàng
-    BaiTapGameXucXacReducer  //state baì tập game xúc xắc
+    BaiTapGameXucXacReducer,  //state baì tập game xúc xắc
+    BaiTapOanTuXiReducer
 })
 
 export default rootReducer;
